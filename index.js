6b425932316bb545ce24e327d7eb19d2989eaccc
tomgp <tom.g.pearson@gmail.com>
2015-09-11 18:20:54 +0100

    App up and running, templates and data in place though not yet talking to one another

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const config = require('./config/index.js');
+const app = express();
+const handlebars = require('express-handlebars');
+
+app.engine('.hbs', handlebars());
+
+app.set('view engine', '.hbs');
+
+app.route('/economic-dashboard/:area')
+	.get(function(req, res) {
+		let area = req.params.area.toLowerCase();
+		console.log('c', config.get(area) );
+		if(config.has(area)){
+			res.render( 'main', config.get(area) );
+		}else{
+			res.send('nope');
+		}
+	});
+
+app.get('/', function (req, res) {
+	res.send('Hello World!!');
+});
+
+var server = app.listen(3000, function () {
+	let host = server.address().address;
+	let port = server.address().port;
+
+	console.log( 'Example app listening at http://', server.address() );
+});
