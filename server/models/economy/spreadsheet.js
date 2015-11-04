a347d54a86d94a9bb5db786a0f021ab05eb5a85a
luke.kavanagh <Luke.Kavanagh@ft.com>
2015-11-04 14:31:02 +0000

    Refactor Scss and spreadsheet (ie the dashboard model).
    Also inlcudes
    * Bertha on Https
    * Japan, China and Test dasboards (only available locally at the mo)
    * Bump Gems
    * Tweak Scss linting.

--- server/models/economy/spreadsheet.js
+++ server/models/economy/spreadsheet.js
@@ -1,13 +1,13 @@
 'use strict';
 
 exports.dashboard_data_url = (key, republish) => 
-  'http://bertha.ig.ft.com/' + (republish ? 'republish' : 'view') + '/publish/gss/' + key + '/data,credits,groups,options';
+  'https://bertha.ig.ft.com/' + (republish ? 'republish' : 'view') + '/publish/gss/' + key + '/data,credits,groups,options';
 
 exports.create_dashboard = spreadsheet => {
 
   let options = spreadsheet.options.reduce(map_reduce_options, {});
   let cards = spreadsheet.data;
-  let cards_by_group = cards.reduce(index_card_groups, {});
+  let cards_by_group = cards.reduce(index_by_group, {});
 
   let dashboard = {};
   dashboard.groups = build_groups(spreadsheet.groups, cards_by_group);
@@ -25,54 +25,36 @@ exports.create_dashboard = spreadsheet => {
       description: options['og:description'] && options['og:description'].text,
       image: options['og:image'] && options['og:image'].text
     }
-  }
+  };
 
   return dashboard;
 };
-
-// res.locals.dashboard = economies[economy].data;
-//     res.locals.page = economies[economy].page;
-// res.locals.dashboard = economies[economy].data = data;
-//         res.locals.page = {
-//           title: res.locals.dashboard.title + ' – FT.com',
-//           description: res.locals.dashboard.description,
-//           social: {
-//             basic: {
-//               title: '21 charts that explain the US economy',
-//               description: res.locals.dashboard.description,
-//             }
-//           }
-//         };
 
 function map_reduce_options(o, row) {
   o[row.name] = {text: row.value, html: row.html || row.value};
   return o;
 }
 
-function index_card_groups(index, row) {
-  let key = row.type;
-  if (index[key]) {
-    index[key].indicators.push(row);
+function index_by_group(index, row) {
+  let id = row.group;
+  if (index[id]) {
+    index[id].push(row);
   } else {
-    index[key] = {
-      name: row.category,
-      type: row.type,
-      indicators: [row],
-      indexcard: row.indexcard
-    };
+    index[id] = [row];
   }
   return index;
 }
 
 function build_groups(groups, card_index) {
-  return groups.map(function(row) {
-    let type = row.type;
-    if (type && this[type]) {
-      let group = this[type];
-      group.name = row.name || group.name;
-      return group;
-    } else {
-      return row;
+  return groups.map(function(group) {
+    let id = group.id;
+    let indicators = id && this[id];
+    if (id && indicators) {
+      group.cards = indicators.map(indicator=>{
+        indicator.group = group;
+        return indicator;
+      });
     }
+    return group;
   }, card_index);
 }
